cd5c5d2e410eb6ec32d1d22541cc06ce5b74a8f8
Squirtllechu23 <Squirtlechu123@gmail.com>
2026-01-15 13:27:53 -0600

    67

--- 67/67/67.cpp
+++ 67/67/67.cpp
@@ -1,33 +1,67 @@
-#include<iostream>
-#include<iomanip>
-#include<ctime>
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <iomanip>
+#include <cmath>
+#include <fstream>
+#include <ctime>
 using namespace std;
-int DIS();
-int File_IO();
-int DA();
-int SF();
-int SOFU();
+
 int main()
 {
+	//Define variable for menu choice
+	int choice = 0;
+	while (choice != 6)
+	{
+		do
+		{
+			//Display the menu for the user
+			cout << "Student Grades System \n"
+				<< "Please make a sleection from the options below:\n";
+			cout << "1. Data Input and Storage\n";
+			cout << "2. File I/O\n";
+			cout << "3. Data Analysis\n";
+			cout << "4. Search Functionality\n";
+			cout << "5. Sorting Functionality\n";
+			cout << "6. Quit\n";
+			cout << ":>";
+			cin >> choice;
+		} while (choice < 1 || choice > 6);
 
-}
-int DIS()
-{
+		//Process the choice
+		switch (choice)
+		{
 
-}
-int File_IO()
-{
+		case 1:
+		{
+			int student;
+			cout << "How many ";
+		}
+		break;
+		case 2:
+		{
 
-}
-int DA()
-{
+		}
+		break;
+		case 3:
+		{
 
-}
-int SF()
-{
+		}
+		break;
+		case 4:
+		{
 
-}
-int SOFU()
-{
-
+		}
+		break;
+		case 5:
+		{
+			cout << "\nnBye Bye";
+		}
+		break;
+		default:
+		{
+			cout << "Invalid CHoice\n\n";
+		}
+		}
+	}
 }
